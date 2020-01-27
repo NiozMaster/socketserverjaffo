9364b1776b1f60d7e362814ad93c83df9e7d31b9
Jesús Fuentes <jaff_7592@yahoo.com>
2020-01-26 22:58:04 -0500

    New configs for Socket Server

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,48 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO  from 'socket.io';
+import http from 'http';
+
+import * as _socket from "../sockets/sockets";
 
 export default class Server {
+
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor(){
-        this.app = express();
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor(){
+        this.app = express(); 
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSocket();
+    }
+
+    public static get instance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSocket(){
+        console.log('Escuchando el socket');
+        this.io.on('connection', cliente => {
+            console.log('nuevo cliente conectado');
+
+            //escuchando mensajes
+            _socket.mensaje(cliente, this.io);
+
+            //desconexion del cliente
+            _socket.desconectar(cliente);
+        })
     }
 
     start(callback:Function) {
-        this.app.listen(this.port, callback());
+        this.httpServer.listen(this.port, callback());
     }
 }
